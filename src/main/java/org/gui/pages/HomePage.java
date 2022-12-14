79fc51a3bf10f8c224c5d42780c0f0c6e674ad2c
NFartenadze <nfartenadze@solvd.com>
2022-12-14 11:34:14 +0400

    initial commit

--- src/main/java/org/gui/pages/HomePage.java
+++ src/main/java/org/gui/pages/HomePage.java
@@ -1,6 +1,5 @@
 package org.gui.pages;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -14,23 +13,22 @@ import java.util.List;
 
 public class HomePage {
     private static final Logger logger = LogManager.getLogger(HomePage.class);
+    WebDriver driver = new ChromeDriver();
 
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://us.puma.com/us/en");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10));
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        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
         //search bar
         WebElement searchBar = driver.findElement(By.xpath("//input['data-test-id=\"search-box\"']"));
         searchBar.sendKeys("Bag");
         searchBar.sendKeys(Keys.ENTER);
-
-//        List<WebElement> searchResult = driver.findElements(By.xpath("//a[@data-test-id=\"product-list-item-link\"]"));
-
         //searched items list
         List<WebElement> searchResult = driver.findElements(By.xpath("//ul[@id=\"product-list-items\"]/li/div[3]/a/h3"));
         searchResult.forEach(e -> logger.info(e.getText()));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 
         driver.quit();
